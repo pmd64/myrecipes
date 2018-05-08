c8f833911de28c6979f0e1210c0ad968ab95c052
Paul Danis <paul.pmmgmt@gmail.com>
2018-05-07 22:52:41 -0400

    added gravatar

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -4,7 +4,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
   
   def setup 
     @chef = Chef.create!(chefname:"mashrur", email:"mashrur@example.com")
-    @recipe = @chef.recipes.build(name:"chicken saute", description:"boil in pot with m&m's")
+    @recipe = @chef.recipes.build(name:"chicken saute", description:"boil in pot with vegetables")
     @recipe.save
     @recipe2 = @chef.recipes.build(name:"baked ziti", description:"make pasta add sauce and cheese and bake")
     @recipe2.save
@@ -19,8 +19,16 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipes listing" do
     get recipes_path
     assert_template 'recipes/index'
-    assert_match @recipe.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
+  end
+  
+  test "should get recipes show" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_match @recipe.name.capitalize, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.chefname, response.body
   end
   
 end
