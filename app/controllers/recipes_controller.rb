388da41dff1b5ff5a636c0d6cf21397cc294a930
Paul Danis <paul.pmmgmt@gmail.com>
2018-05-08 22:28:53 -0400

    added new recipe creation with tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,5 +7,27 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])  
   end
   
+  def new
+    @recipe = Recipe.new
+  end
   
+  def create
+    @recipe = Recipe.create(recipe_params) 
+    @recipe.chef = Chef.first
+    if @recipe.save 
+      flash[:success] = "Recipe was created successfully"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+    
+    
+  end
+  
+  
+  private
+  
+    def recipe_params
+      params.require(:recipe).permit(:name, :description)
+    end
 end
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -31,4 +31,31 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @chef.chefname, response.body
   end
   
+  test "add a new valid recipe"  do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    name_of_recipe = "chicken saute"
+    description_of_recipe = "boil in pot with vegetables"
+    assert_difference 'Recipe.count',1 do 
+      post recipes_path, params: { recipe: { name: name_of_recipe, description: description_of_recipe } } 
+    end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe, response.body
+    
+  end
+  
+  test "reject invalid recipe submissions" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: { name: " ", description: " "} } 
+    end
+    assert_template 'recipes/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+    
+  end
+  
+  
 end
