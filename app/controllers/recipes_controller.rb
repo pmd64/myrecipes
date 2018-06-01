5bce05db3543a8d296954e1d2bfc032249d46771
Paul Danis <paul.pmmgmt@gmail.com>
2018-05-31 20:47:17 -0400

    clean up and nav changes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,7 +15,7 @@ class RecipesController < ApplicationController
   
   def create
     @recipe = Recipe.create(recipe_params) 
-    @recipe.chef = Chef.first
+    @recipe.chef = current_chef
     if @recipe.save 
       flash[:success] = "Recipe was created successfully"
       redirect_to recipe_path(@recipe)
